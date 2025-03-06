91d8cd1c03008b7f1a7d8454d401c07ab2797abd
Justin Acton <jacton@aapg.org>
2025-03-06 14:44:33 -0600

    removed nodejs

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting US States Travel Map server...
-echo.
-echo Once the server is running, open your browser and go to:
-echo http://localhost:3000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-node server.js 
